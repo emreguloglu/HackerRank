63dbf38113179e92241142f333017a7ef593a94e
emreg <emreguloglu@gmail.com>
2020-04-12 14:13:41 +0300

    Day 16: Exceptions - String to Integer

---
+++ src/main/java/challenge/BadStringException.java
@@ -0,0 +1,18 @@
+package challenge;
+
+import java.util.Scanner;
+
+public class BadStringException {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        try {
+            int i = Integer.parseInt(S);
+            System.out.println(i);
+        } catch (Exception e) {
+            System.out.println("Bad String");
+        }
+        in.close();
+    }
+}
